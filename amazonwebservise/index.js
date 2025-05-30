9b7da4889f0e146ed4bac76bf151f1d8f78f5fe5
luciano-ballespin <90703780+luhoballespin@users.noreply.github.com>
2025-05-30 11:22:19 -0300

    subi la carpeta

---
+++ amazonwebservise/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("¡Hola desde tu API en AWS!");
+});
+
+app.get("/saludo", (req, res) => {
+  res.json({ mensaje: "Hola mundo desde /saludo" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
